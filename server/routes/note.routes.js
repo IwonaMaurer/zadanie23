333d6d53d46619975b478569610f0b98f9f315e7
IwonaMaurer <plastimex2@wp.pl>
2018-02-11 17:58:42 +0100

    update

---
+++ server/routes/note.routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import * as NoteController from '../controllers/note.controller';
+
+const router = new Router();
+
+// Add a new Note
+router.route('/notes').post(NoteController.addNote);
+
+// Delete a single note
+router.route('/notes/:noteId').delete(NoteController.deleteNote);
+
+// Edit a note content by noteId
+router.route('/notes').put(NoteController.editNoteContent);
+
+export default router;
